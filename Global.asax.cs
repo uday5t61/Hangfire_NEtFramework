884311cdb5d98481ce55feba609c92c0aeaf21a6
uday5t61 <uday5t61@gmail.com>
2023-05-17 20:18:56 +0530

    Add project files.

---
+++ Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebApplication1
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+ 
+
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            HangfireAspNet.Use(GetHangfireServers);
+
+            // Let's also create a sample background job
+           // BackgroundJob.Enqueue(() => Debug.WriteLine("Hello world from Hangfire!"));
+            //RecurringJob.AddOrUpdate(() => Console.WriteLine("Minutely Job executed"), Cron.Minutely);
+            RecurringJob.AddOrUpdate("powerfuljob", () => Debug.Write($"Powerful! - {DateTime.Now}"), Cron.Minutely);
+            RecurringJob.TriggerJob("powerfuljob");
+        }
+
+        private IEnumerable<IDisposable> GetHangfireServers()
+        {
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage("DBConnectionString");
+
+            yield return new BackgroundJobServer();
+        }
+    }
+}
